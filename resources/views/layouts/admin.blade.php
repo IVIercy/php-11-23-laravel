ff688cf914f326f1e16fe64ac4e92ae574d443c7
Vladyslav Parkhomenko <i1ame050598@gmail.com>
2024-01-22 19:25:05 +0200

    [HW_24] Admin categories

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,8 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <link href="{{ asset('css/iziToast.css') }}" rel="stylesheet">
+
     <!-- Scripts -->
     @vite(['resources/scss/admin/admin.scss', 'resources/js/app.js'])
 </head>
@@ -86,5 +88,7 @@
         </div>
     </main>
 </div>
+<script src="{{ asset('js/iziToast.js') }}"></script>
+@include('vendor.lara-izitoast.toast')
 </body>
 </html>
