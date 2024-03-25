5524a505605439e9443c5ed6fd79106d145756ff
Vladyslav Parkhomenko <i1ame050598@gmail.com>
2024-03-25 15:54:35 +0200

    [HW_34] AWS

--- database/migrations/2024_03_13_134832_create_wish_list_table.php
+++ database/migrations/2024_03_13_134832_create_wish_list_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('wish_list', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('user_id')->constrained('users');
+            $table->boolean('price')->default(false);
+            $table->boolean('exist')->default(false);
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Order;
-use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('test', function() {
-    $user = auth()->user();
-    $product = Product::find(41);
-
-    dd($user->wishes()->where('product_id', $product->id)->wherePivot('exist', false)->exists());
+    $user = \App\Models\User::find(1);
+    \App\Events\UserNotify::dispatch($user, 'test message for user = ' . $user->id);
 });
 Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
 
@@ -61,6 +57,7 @@ Route::middleware(['auth'])->group(function() {
     Route::get('invoices/{order}', \App\Http\Controllers\InvoiceController::class)->name('invoice');
     Route::post('wishlist/{product}', [\App\Http\Controllers\WishListController::class, 'add'])->name('wishlist.add');
     Route::delete('wishlist/{product}', [\App\Http\Controllers\WishListController::class, 'remove'])->name('wishlist.remove');
+    Route::get('account/wishlist', \App\Http\Controllers\Account\WishlistController::class)->name('account.wishlist');
 });
 
 Route::name('callbacks.')->prefix('callback')->group(function() {
